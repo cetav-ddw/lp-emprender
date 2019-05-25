fdcc43c4a45a4b1d61c458e650d46ca063a372c7
JgeovanniAm <geovanniam171078@gmail.com>
2019-05-25 17:56:25 -0600

    update: change styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 (function () {
 	const chatbtnArray = document.querySelectorAll('#chatbot-js');
-	const windowChatBot = document.querySelector('.article-chatbot__chat-field');
+	const windowChatBot = document.querySelector('.chat-field');
 	const closebtn = document.querySelector('#btnclose-js');
 	
 	chatbtnArray.forEach(element => {
@@ -8,16 +8,10 @@
 	});
 	closebtn.addEventListener('click', closeWindow);
 	function showWindow() {
-		windowChatBot.style.right = "0px";
-		windowChatBot.style.transform = "scale(1)"
-		windowChatBot.style.opacity = '1';
-		windowChatBot.style.transition = "ease-in-out 1.2s";
+		windowChatBot.className = "open-window"
 	}
 
 	function closeWindow(){
-		windowChatBot.style.right = "-40vw";
-		windowChatBot.style.transform = "scale(0)";
-		windowChatBot.style.opacity = '0';
-		windowChatBot.style.transition = "ease-in-out 1.2s";
+		windowChatBot.className = "chat-field"
 	}
 }())
